f0c12998378e022a51a85a1b509d4131918612dd
ImpulsiveLad <scpkingriley@gmail.com>
2024-02-27 17:55:37 -0500

    Add project files.

---
+++ Arachnophilia/Plugin.cs
@@ -0,0 +1,124 @@
+﻿using Arachnophilia.Patches;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using CSync.Lib;
+using CSync.Util;
+using HarmonyLib;
+using System.Runtime.Serialization;
+
+namespace Arachnophilia
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Arachnophilia : BaseUnityPlugin
+    {
+        private const string modGUID = "impulse.Arachnophilia";
+        private const string modName = "Arachnophilia";
+        private const string modVersion = "1.6.0";
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public ManualLogSource mls;
+
+        public static Arachnophilia instance;
+
+        public new static SyncConfig Config;
+        void Awake()
+        {
+            instance = this;
+
+            Config = new SyncConfig(base.Config);
+
+            harmony.PatchAll(typeof(SandSpiderStartPatch));
+            harmony.PatchAll(typeof(SandSpiderAttemptPlaceWebTrapPatch));
+            harmony.PatchAll(typeof(SandSpiderState));
+            harmony.PatchAll(typeof(SandSpiderUpdatePatch));
+            harmony.PatchAll(typeof(SandSpiderUpdateTPatch));
+            harmony.PatchAll(typeof(SandSpiderOnCollideWithPlayerPatch));
+            harmony.PatchAll(typeof(SandSpiderGetWallPositionForSpiderMeshPatch));
+            harmony.PatchAll(typeof(SandSpiderAIGrabBodyPatch));
+            harmony.PatchAll(typeof(SyncConfig));
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+        }
+    }
+    [DataContract]
+    public class SyncConfig : SyncedConfig<SyncConfig>
+    {
+        [DataMember] public SyncedEntry<int> MinWebCount { get; private set; }
+        [DataMember] public SyncedEntry<int> MaxWebCount { get; private set; }
+        [DataMember] public SyncedEntry<float> MinWebLength { get; private set; }
+        [DataMember] public SyncedEntry<float> MaxWebLength { get; private set; }
+        [DataMember] public SyncedEntry<float> MinWebDistance { get; private set; }
+        [DataMember] public SyncedEntry<float> SpiderSetupSpeed { get; private set; }
+        [DataMember] public SyncedEntry<float> SpiderChaseSpeed { get; private set; }
+        [DataMember] public SyncedEntry<float> WebTimeRange { get; private set; }
+        [DataMember] public SyncedEntry<float> FailedWebTrapTime { get; private set; }
+        [DataMember] public SyncedEntry<float> WebPlaceInterval { get; private set; }
+        [DataMember] public SyncedEntry<int> SpiderDamage { get; private set; }
+        [DataMember] public SyncedEntry<int> SpiderHp { get; private set; }
+        [DataMember] public SyncedEntry<int> MinSpooledBodies { get; private set; }
+        [DataMember] public SyncedEntry<int> MaxSpooledBodies { get; private set; }
+
+        public SyncConfig(ConfigFile cfg) : base("CustomDeathPenalty")
+        {
+            ConfigManager.Register(this);
+
+            SpiderSetupSpeed = cfg.BindSyncedEntry("General",
+            "Spider Setup Speed",
+            1.5f,
+            "Speed multiplier applied to the spider while it is not chasing a player.");
+            SpiderChaseSpeed = cfg.BindSyncedEntry("General",
+                        "Spider Chase Speed",
+                        1.25f,
+                        "Speed multiplier applied to the spider while it is chasing a player.");
+            SpiderDamage = cfg.BindSyncedEntry("General",
+                        "Spider Damage",
+                        35,
+                        "The damage dealt to players by the spider.");
+            SpiderHp = cfg.BindSyncedEntry("General",
+                        "Spider HP",
+                        6,
+                        "Health of the spider.");
+            MinSpooledBodies = cfg.BindSyncedEntry("Features",
+                        "Min Extra Cocoons",
+                        3,
+                        "The minimum number of bodies the spider spawns in and decorates its nest with during its setup phase. Set both min and max to 0 to disable. Keep in mind that this value should be higher than Min Web Count, excessively high values may result in lag, Min should be less than Max.");
+            MaxSpooledBodies = cfg.BindSyncedEntry("Features",
+                        "Max Extra Cocoons",
+                        4,
+                        "The maximum number of bodies the spider spawns in and decorates its nest with during its setup phase. Set both min and max to 0 to disable. Keep in mind that this value should be higher than Min Web Count, excessively high values may result in lag, Min should be less than Max.");
+            MinWebCount = cfg.BindSyncedEntry("Webs",
+                        "Min Web Count",
+                        20,
+                        "The minimum amount of webs a given spider can place, Min should be less than Max.");
+            MaxWebCount = cfg.BindSyncedEntry("Webs",
+                        "Max Web Count",
+                        25,
+                        "The maximum amount of webs a given spider can place, Min should be less than Max.");
+            MinWebLength = cfg.BindSyncedEntry("Webs",
+                        "Min Web Length",
+                        2f,
+                        "The minimum length a web can be in units, !! Keep in mind that the smaller the difference between the min and max length, the less spots a spider could successfully place a web!!");
+            MaxWebLength = cfg.BindSyncedEntry("Webs",
+                        "Max Web Length",
+                        10f,
+                        "The maximum length a web can be in units, !! Keep in mind that the smaller the differnce between the min and max length, the less spots a spider could successfully place a web!!");
+            MinWebDistance = cfg.BindSyncedEntry("Webs",
+                        "Min Distance Between Webs",
+                        0.5f,
+                        "This controls the distance in units a spider must be from the closest web in order to place a new one, a lower value will make webs closely knit, a higher value will make webs spaced further apart.");
+            WebPlaceInterval = cfg.BindSyncedEntry("Webs",
+                       "Web Placement Interval",
+                       2f,
+                       "How long in seconds the spider should wait after placing a web to place another");
+            WebTimeRange = cfg.BindSyncedEntry("Webs",
+                        "Web Placement Interval Variation",
+                        0.5f,
+                        "This increases the interval and gives it some variation, the Web Placement Interval plus this value will be the max interval, the min interval will be the Web Placement Interval plus half of this value. (2 & 0.5 for these settings will result in a 2.25-2.5 second interval, 10 & 7 will result in a 13.5-17 second interval. etc)");
+            FailedWebTrapTime = cfg.BindSyncedEntry("Webs",
+                        "Failed Web Placement Cooldown",
+                        0.1f,
+                        "How long in seconds spider should wait after failing to place a web before attempting to place another.");
+        }
+    }
+}
---
+++ Arachnophilia/SandSpiderAIPatch.cs
@@ -0,0 +1,301 @@
+﻿using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Arachnophilia.Patches
+{
+    [HarmonyPatch(typeof(SandSpiderAI))]
+    [HarmonyPatch("Start")]
+    internal class SandSpiderStartPatch
+    {
+        static void Postfix(SandSpiderAI __instance)
+        {
+            __instance.maxWebTrapsToPlace = Random.Range(SyncConfig.Instance.MinWebCount.Value - 1 - SyncConfig.Instance.MinSpooledBodies.Value * 1 / 3, SyncConfig.Instance.MaxWebCount.Value - SyncConfig.Instance.MaxSpooledBodies.Value * 1 / 2);
+            __instance.enemyHP = SyncConfig.Instance.SpiderHp.Value;
+        }
+    }
+    [HarmonyPatch(typeof(SandSpiderAI))]
+    [HarmonyPatch("AttemptPlaceWebTrap")]
+    internal class SandSpiderAttemptPlaceWebTrapPatch : GrabbableObject
+    {
+        static bool Prefix(SandSpiderAI __instance)
+        {
+            for (int i = 0; i < __instance.webTraps.Count; i++)
+            {
+                if (Vector3.Distance(__instance.webTraps[i].transform.position, __instance.abdomen.position) < SyncConfig.Instance.MinWebDistance.Value)
+                {
+                    return false;
+                }
+            }
+            float x = Random.Range(-1f, 1f);
+            float z = Random.Range(-1f, 1f);
+            float y = Random.Range(0, 1f);
+            Vector3 direction = new Vector3(x, y, z).normalized;
+            Ray ray = new Ray(__instance.abdomen.position + Vector3.up * 0.4f, direction);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, SyncConfig.Instance.MaxWebLength.Value, StartOfRound.Instance.collidersAndRoomMask))
+            {
+                if (rayHit.distance < SyncConfig.Instance.MinWebLength.Value)
+                {
+                    return false;
+                }
+                Arachnophilia.instance.mls.LogInfo($"Current time: {System.DateTime.Now} - Got spider web raycast; end point: {rayHit.point}; {rayHit.distance}");
+                Vector3 point = rayHit.point;
+                if (Physics.Raycast(__instance.abdomen.position, Vector3.down, out rayHit, 10f, StartOfRound.Instance.collidersAndRoomMask))
+                {
+                    Vector3 startPosition = rayHit.point + Vector3.up * 0.2f;
+                    __instance.SpawnWebTrapServerRpc(startPosition, point);
+                }
+            }
+            return false;
+        }
+    }
+    internal class SandSpiderState
+    {
+        public Dictionary<SandSpiderAI, int> bodyCounts = new Dictionary<SandSpiderAI, int>();
+        public Dictionary<SandSpiderAI, bool> coroutineStartedFlags = new Dictionary<SandSpiderAI, bool>();
+        public Dictionary<SandSpiderAI, int> randomSpooledBodies = new Dictionary<SandSpiderAI, int>();
+    }
+    [HarmonyPatch(typeof(SandSpiderAI))]
+    [HarmonyPatch("Update")]
+    internal class SandSpiderUpdatePatch
+    {
+        static SandSpiderState state = new SandSpiderState();
+        static void Postfix(SandSpiderAI __instance)
+        {
+            lock (state.bodyCounts)
+            {
+                lock (state.coroutineStartedFlags)
+                {
+                    if (!state.bodyCounts.ContainsKey(__instance))
+                    {
+                        state.bodyCounts[__instance] = 0;
+                        state.coroutineStartedFlags[__instance] = false;
+                        state.randomSpooledBodies[__instance] = Random.Range(SyncConfig.Instance.MinSpooledBodies.Value, SyncConfig.Instance.MaxSpooledBodies.Value + 1);
+                    }
+                }
+            }
+            int randomSpooledBodies = state.randomSpooledBodies[__instance];
+            float webSettingSpeedMultiplier = SyncConfig.Instance.SpiderSetupSpeed.Value;
+            float playerChasingSpeedMultiplier = SyncConfig.Instance.SpiderChaseSpeed.Value;
+            switch (__instance.currentBehaviourStateIndex)
+            {
+                case 0:
+                    __instance.agent.speed *= webSettingSpeedMultiplier;
+                    __instance.spiderSpeed *= webSettingSpeedMultiplier;
+                    lock (state.coroutineStartedFlags)
+                    {
+                        state.coroutineStartedFlags[__instance] = false;
+                    }
+                    break;
+                case 1:
+                    __instance.agent.speed *= webSettingSpeedMultiplier;
+                    __instance.spiderSpeed *= webSettingSpeedMultiplier;
+                    lock (state.bodyCounts)
+                    {
+                        lock (state.coroutineStartedFlags)
+                        {
+                            if (state.bodyCounts[__instance] < randomSpooledBodies && !state.coroutineStartedFlags[__instance])
+                            {
+                                __instance.StartCoroutine(InstantiateRagdoll(__instance));
+                                state.coroutineStartedFlags[__instance] = true;
+                            }
+                        }
+                    }
+                    break;
+                case 2:
+                    __instance.agent.speed *= playerChasingSpeedMultiplier;
+                    __instance.spiderSpeed *= playerChasingSpeedMultiplier;
+                    if (__instance.currentlyHeldBody != null)
+                    {
+                        __instance.CancelSpoolingBody();
+                    }
+                    lock (state.coroutineStartedFlags)
+                    {
+                        state.coroutineStartedFlags[__instance] = false;
+                    }
+                    break;
+            }
+        }
+        static IEnumerator InstantiateRagdoll(SandSpiderAI __instance)
+        {
+            while (__instance.isOutside)
+            {
+                yield return null;
+            }
+            while (__instance.currentBehaviourStateIndex == 2)
+            {
+                yield return null;
+            }
+            while (__instance.currentlyHeldBody != null)
+            {
+                yield return null;
+            }
+            yield return new WaitForSeconds(3f);
+            if (__instance.isEnemyDead)
+            {
+                yield break;
+            }
+            GameObject ragdollPrefab = StartOfRound.Instance.playerRagdolls[0];
+            GameObject ragdoll = Object.Instantiate(ragdollPrefab, __instance.homeNode.position, Quaternion.identity);
+            DeadBodyInfo deadBodyInfo = ragdoll.GetComponent<DeadBodyInfo>();
+            ragdoll.AddComponent<NetworkObject>();
+            Transform bodyScanNode = ragdoll.transform.Find("ScanNode");
+            if (bodyScanNode != null)
+            {
+                bodyScanNode.gameObject.SetActive(false);
+            }
+            Transform bodyMapDot = ragdoll.transform.Find("MapDot");
+            if (bodyMapDot != null)
+            {
+                bodyMapDot.gameObject.SetActive(false);
+            }
+            BoxCollider bodyBoxCollider = ragdoll.GetComponent<BoxCollider>();
+            if (bodyBoxCollider != null)
+            {
+                bodyBoxCollider.enabled = false;
+            }
+            __instance.spooledPlayerBody = false;
+            __instance.decidedChanceToHangBodyEarly = false;
+            __instance.turnBodyIntoWebCoroutine = null;
+            if (__instance.currentlyHeldBody != null)
+            {
+                __instance.SpiderTurnBodyIntoWebServerRpc();
+            }
+            if (__instance.currentlyHeldBody != null)
+            {
+                __instance.SpiderHangBodyServerRpc();
+            }
+            __instance.GrabBody(deadBodyInfo);
+            Arachnophilia.instance.mls.LogInfo($"Current time: {System.DateTime.Now} - Spoolable prey instantiated");
+            lock (state.bodyCounts)
+            {
+                lock (state.coroutineStartedFlags)
+                {
+                    state.bodyCounts[__instance]++;
+                    state.coroutineStartedFlags[__instance] = false;
+                }
+            }
+            if (!__instance.spoolingPlayerBody)
+            {
+                __instance.currentBehaviourStateIndex = 0;
+            }
+        }
+    }
+    [HarmonyPatch(typeof(SandSpiderAI), "Update")]
+    public static class SandSpiderUpdateTPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_R4)
+                {
+                    float operandValue = (float)codes[i].operand;
+
+                    if (operandValue == 4f)
+                    {
+                        codes[i].operand = SyncConfig.Instance.WebPlaceInterval.Value;
+                    }
+                    else if (operandValue == 0.5f)
+                    {
+                        codes[i].operand = SyncConfig.Instance.WebTimeRange.Value / 2;
+                    }
+                    else if (operandValue == 1f)
+                    {
+                        codes[i].operand = SyncConfig.Instance.WebTimeRange.Value;
+                    }
+                    else if (operandValue == 0.17f)
+                    {
+                        codes[i].operand = SyncConfig.Instance.FailedWebTrapTime.Value;
+                    }
+                }
+            }
+            return codes;
+        }
+    }
+    [HarmonyPatch(typeof(SandSpiderAI), "OnCollideWithPlayer")]
+    public static class SandSpiderOnCollideWithPlayerPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_S && (sbyte)codes[i].operand == 90)
+                {
+                    codes[i].operand = (sbyte)SyncConfig.Instance.SpiderDamage.Value;
+                }
+            }
+            return codes.AsEnumerable();
+        }
+    }
+    [HarmonyPatch(typeof(SandSpiderAI))]
+    [HarmonyPatch("GetWallPositionForSpiderMesh")]
+    public static class SandSpiderGetWallPositionForSpiderMeshPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_R4)
+                {
+                    float operandValue = (float)codes[i].operand;
+
+                    if (operandValue == 6f)
+                    {
+                        codes[i].operand = 1.5f;
+                    }
+                    else if (operandValue == 10f)
+                    {
+                        codes[i].operand = 100f;
+                    }
+                    else if (operandValue == 10.1f)
+                    {
+                        codes[i].operand = 100.1f;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+    }
+    [HarmonyPatch(typeof(SandSpiderAI))]
+    [HarmonyPatch("GrabBody")]
+    public static class SandSpiderAIGrabBodyPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Call && (MethodInfo)codes[i].operand == AccessTools.Method(typeof(SandSpiderAI), "GrabBody"))
+                {
+                    codes.Insert(i, new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(SandSpiderAIGrabBodyPatch), "ModifiedGrabBody")));
+                    break;
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        public static void ModifiedGrabBody(SandSpiderAI __instance, DeadBodyInfo body)
+        {
+            if (!__instance.isOutside)
+            {
+                __instance.currentlyHeldBody = body;
+                __instance.currentlyHeldBody.attachedLimb = __instance.currentlyHeldBody.bodyParts[6];
+                __instance.currentlyHeldBody.attachedTo = __instance.mouthTarget;
+                __instance.currentlyHeldBody.matchPositionExactly = true;
+            }
+        }
+    }
+}
